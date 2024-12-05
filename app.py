0e639e832cd37e442b2368c07474923c2934f5c5
Olzhas <olzhasmaisuly@gmail.com>
2024-12-05 09:57:51 +0500

    Added port binding for Render

--- app.py
+++ app.py
@@ -168,3 +168,6 @@ def delete_ad(ad_id):
 
     
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Используем порт из переменной окружения или 5000 по умолчанию
+    app.run(host='0.0.0.0', port=port) 
